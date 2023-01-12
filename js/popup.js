b57df732286a425c1aecc235c70fe9a364c5b2e2
Vito Su <vitosu@qt-asia.com>
2023-01-12 13:13:16 +0800

    调整样式

--- js/popup.js
+++ js/popup.js
@@ -10,127 +10,121 @@ class Popup {
         this.closeEvent = null;
     }
     create() {
-        let style = $("<style scoped></style>");
-        style.html(`
-.miner{
-    width: 550px;
-    height: 660px;
-    border: 1px solid grey;
-    position: fixed;
-    z-index: 999; 
-    border-radius: 5px; 
-    background-color: white;
-    margin: 5px;
-}
-.miner .tabInfo{
-    padding: 6px 0px; 
-    text-align: left; 
-    background-color: #E5F2F2;
-    border-radius: 5px;
-    height: 30px;
-}
-.miner .tabInfo div{
-    background-color: #E5F2F2;
-    padding-bottom: 5px;
-}
-.miner .titleInfo{
-    float: left; 
-    font-size:18px; 
-    cursor: pointer;
-    color: #9999;
-    margin-left:10px;
-}
-.miner .tabInfo .downloadButton{
-    text-align: right; 
-    margin: 2px 2px 0px 0px;
-    padding-right: 10px;
-}
-.miner .tabInfo .downloadButton img{
-    width: 25px;
-    height: 25px;
-    cursor: pointer;
-}
-.miner .infoContent{
-	padding: 10px 8px; 
-    overflow-y: auto; 
-    overflow-x: hidden; 
-    height: 620px; 
-    clear: both;
-    font-family: Helvetica, arial, freesans, clean, sans-serif;
-    font-size: 14px;
-    color: #333;
-}
-.miner .title{
-	height:20px;
-	line-height:20px;	
-	color:#3f51b5;
-	font-size:20px;
-    margin-bottom: 0px;
-}
-.miner .title::before { 
-	content: "●"; 
-	font-size: 40px;
-	line-height:15px;
-	color:#3f51b5;
-	margin-right:5px;
-    box-sizing: revert;
-}
-.miner .title::after {
-    content: " »";
-    font-size: 20px;
-	line-height: 15px;
-	color: #3f51b5;
-	margin-right: 5px;
-    box-sizing: revert;
-}
-.miner .lineContainer{
-    
-}
-.miner .line{
-    font-size: 14px;
-	margin-left: 12px;
-	padding-left: 20px;
-	line-height: 30px;
-	border-left-color: #999999;
-	border-left-width: 1px;
-	border-left-style: solid;
-    word-wrap: break-word;
-}
-.miner .line:first-child{
-	padding-top:5px;
-}
-.miner .line:last-child{
-	padding-bottom:5px;
-}
-.miner .line span{
-    cursor: pointer;
-}
-.miner .line img{
-    float: left;
-    padding-right: 5px;
-    padding-top: 8px;
-}
-.miner .line input[type="checkbox"]{
-    height: 0px; 
-    width: 0px;
-    padding: 0px;
-    margin: 0px;
-    border: 0px;
-}
-.miner .infoContent::-webkit-scrollbar {
-  width: 5px; 
-}
-.miner .infoContent::-webkit-scrollbar-thumb {
-    background: #ccc; 
-    border-radius: 5px;
-}
+        let style = $(`<style scoped>
+                    .miner{
+                        width: 550px;
+                        height: 660px;
+                        border: 1px solid grey;
+                        position: fixed;
+                        z-index: 999; 
+                        border-radius: 5px; 
+                        background-color: white;
+                        margin: 5px;
+                    }
+                    .miner .tabInfo{
+                        padding: 6px 0; 
+                        text-align: left; 
+                        background-color: #E5F2F2;
+                        border-radius: 5px;
+                        height: 30px;
+                    }
+                    .miner .tabInfo div{
+                        background-color: #E5F2F2;
+                    }
+                    .miner .titleInfo{
+                        float: left; 
+                        font-size:18px; 
+                        cursor: pointer;
+                        color: #9999;
+                        margin-left: 10px;
+                    }
+                    .miner .tabInfo .downloadButton{
+                        text-align: right; 
+                        
+                        padding-right: 10px;
+                    }
+                    .miner .tabInfo .downloadButton img{
+                        width: 25px;
+                        height: 25px;
+                        cursor: pointer;
+                    }
+                    .miner .infoContent{
+                        padding: 10px 8px; 
+                        overflow-y: auto; 
+                        overflow-x: hidden; 
+                        height: 595px; 
+                        clear: both;
+                        font-family: Helvetica, arial, freesans, clean, sans-serif;
+                        font-size: 14px;
+                        color: #333;
+                    }
+                    .miner .title{
+                        height: 20px;
+                        line-height: 20px;	
+                        color: #3f51b5;
+                        font-size: 20px;
+                        margin-bottom: 0;
+                    }
+                    .miner .title::before { 
+                        content: "●"; 
+                        font-size: 40px;
+                        line-height: 15px;
+                        color: #3f51b5;
+                        margin-right: 5px;
+                        box-sizing: revert;
+                    }
+                    .miner .title::after {
+                        content: " »";
+                        font-size: 20px;
+                        line-height: 15px;
+                        color: #3f51b5;
+                        margin-right: 5px;
+                        box-sizing: revert;
+                    }
+                    .miner .line{
+                        font-size: 14px;
+                        margin-left: 12px;
+                        padding-left: 20px;
+                        line-height: 30px;
+                        border-left-color: #999999;
+                        border-left-width: 1px;
+                        border-left-style: solid;
+                        word-wrap: break-word;
+                    }
+                    .miner .line:first-child{
+                        padding-top:5px;
+                    }
+                    .miner .line:last-child{
+                        padding-bottom: 5px;
+                    }
+                    .miner .line span{
+                        cursor: pointer;
+                    }
+                    .miner .line img{
+                        float: left;
+                        padding-right: 5px;
+                        padding-top: 8px;
+                    }
+                    .miner .line input[type="checkbox"]{
+                        height: 0; 
+                        width: 0;
+                        padding: 0;
+                        margin: 0;
+                        border: 0;
+                    }
+                    .miner .infoContent::-webkit-scrollbar {
+                      width: 5px; 
+                    }
+                    .miner .infoContent::-webkit-scrollbar-thumb {
+                        background: #ccc; 
+                        border-radius: 5px;
+                    }
+                    </style>`);
 
-`);
-
-        const screen_width = document.body.clientWidth;
+        const screen_width = window.innerWidth;
         const screen_height = window.innerHeight;
-        const X = (screen_width - 500) / 2;
-        const Y = (screen_height - 650) / 2;
+        const X = (screen_width - 550) / 2;
+        const Y = (screen_height - 660) / 2;
 
         this.container = $("<div class='miner'></div>");
         this.container.css("left", X + "px");
